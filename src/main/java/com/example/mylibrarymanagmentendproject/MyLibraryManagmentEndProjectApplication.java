f133c2bd3fe890e47fe6b04d8381372426e2642d
adamk <adamkruglak@gmail.com>
2022-02-05 12:12:29 +0100

    Initial commit

---
+++ src/main/java/com/example/mylibrarymanagmentendproject/MyLibraryManagmentEndProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.mylibrarymanagmentendproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLibraryManagmentEndProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyLibraryManagmentEndProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mylibrarymanagmentendproject/MyLibraryManagmentEndProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mylibrarymanagmentendproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLibraryManagmentEndProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
